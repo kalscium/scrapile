22d8e3840eb73ac2f89c2fb9c8ffa87a7380d15d
kalscium <kalscium@protonmail.com>
2024-09-14 11:54:06 +1000

    stmt enum: changed statement back into an enum for other statements

--- src/lang/parser/stmt.rs
+++ src/lang/parser/stmt.rs
@@ -4,7 +4,9 @@ use crate::lang::{error::parser::Error, token::Token, Spanned};
 use super::expr::{parse_expr, Expr};
 
 #[derive(Debug, Clone)]
-pub struct Stmt(pub Expr);
+pub enum Stmt {
+    Expr(Expr),
+}
 
 pub fn parse_stmt(first_tok: Option<Spanned<Result<Token, Error>>>, tokens: &mut SpannedIter<'_, Token>) -> Result<(Spanned<Stmt>, Option<Spanned<Result<Token, Error>>>), Vec<KError<Error>>> {
     let (first_tok, start_span) = match first_tok {
@@ -15,6 +17,6 @@ pub fn parse_stmt(first_tok: Option<Spanned<Result<Token, Error>>>, tokens: &mut
 
     match first_tok {
         // assume that the statement is a expr
-        _ => parse_expr(Some((Ok(first_tok), start_span.clone())), tokens).map(|(expr, next_tok)| ((Stmt(expr.clone()), start_span.start..expr.span.end), next_tok.map(|(tok, span)| (Ok(tok), span)))),
+        _ => parse_expr(Some((Ok(first_tok), start_span.clone())), tokens).map(|(expr, next_tok)| ((Stmt::Expr(expr.clone()), start_span.start..expr.span.end), next_tok.map(|(tok, span)| (Ok(tok), span)))),
     }
 }
--- src/lang/typed/block.rs
+++ src/lang/typed/block.rs
@@ -1,4 +1,4 @@
-use crate::lang::{error::typed::Error, parser::block::Block, typed::{expr, types::Type}, Spanned};
+use crate::lang::{error::typed::Error, parser::{block::Block, stmt::Stmt}, typed::{expr, types::Type}, Spanned};
 use super::{expr::TExpr, symbol_table::TypeTable, types::Typed};
 
 /// A tree representation of a block
@@ -9,20 +9,22 @@ pub struct TBlock {
 }
 
 /// Adds type annotations to a block
-pub fn wrap_block(block: Block, _type_table: &TypeTable) -> Result<Typed<TBlock>, Error> {
+pub fn wrap_block(block: Block, type_table: &TypeTable) -> Result<Typed<TBlock>, Error> {
     // iterate through the block's statements and add type annotations to all of them
     let mut stmts = Vec::new();
     for (stmt, _) in block.stmts {
-        stmts.push(
-            expr::wrap_expr(&stmt.0.asa, _type_table)?.0
-        );
+        stmts.push(match &stmt {
+            Stmt::Expr(expr) => expr::wrap_expr(&expr.asa, type_table)?.0,
+        });
     }
 
     // check the type of the tail, (if there is one)
     let (tail, tail_type) = match block.tail {
         None => (None, Type::Nil),
         Some((stmt, _)) => {
-            let (stmt, _) = expr::wrap_expr(&stmt.0.asa, _type_table)?;
+            let (stmt, _) = match &stmt {
+                Stmt::Expr(expr) => expr::wrap_expr(&expr.asa, type_table)?,
+            };
             let stmt_type = stmt.1.clone();
 
             (Some(stmt), stmt_type)
