0e183cc248b5aa913dd4b85f7cb1d3443607cca9
kalscium <kalscium@protonmail.com>
2024-09-16 19:05:53 +1000

    optimisations: added some optimisations to reduce the size of the scratch binary

--- src/lang/targets/scratch.rs
+++ src/lang/targets/scratch.rs
@@ -3,21 +3,19 @@ use crate::{lang::typed::{expr::TExpr, root::Project, stmt::TStmt, types::Type},
 /// Translates a project into scratch assembly
 pub fn translate(project: Project) -> Assembly {
     let mut stmts = vec![Statement::ClearList { ident: "console".to_string() }]; // first statement is to clear the console
-    let mut variables = Vec::new();
-    let mut lists = Vec::new();
     
     // translate the main procedure's statements
     for stmt in project.main.0.stmts {
-        tstmt(stmt.0.0, &mut variables, &mut lists, &mut stmts);
+        tstmt(stmt.0.0, &mut stmts);
     }
     if let Some(stmt) = project.main.0.tail {
-        tstmt(stmt.0.0, &mut variables, &mut lists, &mut stmts);
+        tstmt(stmt.0.0, &mut stmts);
     }
 
     Assembly {
         stmts,
-        variables,
-        lists: Vec::new(),
+        variables: Vec::new(),
+        lists: vec!["console".to_string()],
         procedures: Vec::new(),
     }
 }
@@ -25,31 +23,27 @@ pub fn translate(project: Project) -> Assembly {
 const NIL: &str = "<nil>";
 
 /// Translates a statement
-pub fn tstmt(stmt: TStmt, variables: &mut Vec<String>, lists: &mut Vec<String>, stmts: &mut Vec<Statement>) {
+pub fn tstmt(stmt: TStmt, stmts: &mut Vec<Statement>) {
     match stmt {
-        TStmt::Expr(expr) => {texpr(expr, variables, lists, stmts);},
+        TStmt::Expr(expr) => {texpr(expr, stmts);},
         TStmt::VarDeclare { ident, value } => {
             // if it's a list then declare the list by making an empty list then setting all the values in it
             if let Type::List(_) = value.1 {
-                lists.push(ident.clone());
-
                 // pull the expressions from the list
                 let exprs = match value.0.0 {
                     TExpr::List(_, exprs) => exprs,
                     TExpr::VarGet { .. } => panic!("todo: need to implement loops first"),
-                    _ => unreachable!()
+                    _ => unreachable!(),
                 };
 
                 // iterate through the exprs and add them to the list
                 for (i, expr) in exprs.into_iter().enumerate() {
-                    let expr = texpr(expr.0, variables, lists, stmts);
+                    let expr = texpr(expr.0, stmts);
                     let stmt = Statement::InsertList { ident: ident.clone(), value: expr, idx:Expr::PosInteger(i as u32 + 1)  };
                     stmts.push(stmt);
                 }
             } else {
-                variables.push(ident.clone());
-
-                let stmt = Statement::SetVar { ident, value: texpr(value.0.0, variables, lists, stmts) };
+                let stmt = Statement::SetVar { ident, value: texpr(value.0.0, stmts) };
                 stmts.push(stmt);
             }
             
@@ -69,12 +63,12 @@ pub fn tstmt(stmt: TStmt, variables: &mut Vec<String>, lists: &mut Vec<String>, 
 
                 // iterate through the exprs and add them to the list
                 for (i, expr) in exprs.into_iter().enumerate() {
-                    let expr = texpr(expr.0, variables, lists, stmts);
+                    let expr = texpr(expr.0, stmts);
                     let stmt = Statement::InsertList { ident: ident.clone(), value: expr, idx:Expr::PosInteger(i as u32 + 1)  };
                     stmts.push(stmt);
                 }
             } else {
-                let stmt = Statement::SetVar { ident, value: texpr(value.0.0, variables, lists, stmts) };
+                let stmt = Statement::SetVar { ident, value: texpr(value.0.0, stmts) };
                 stmts.push(stmt);
             }
         },
@@ -82,7 +76,7 @@ pub fn tstmt(stmt: TStmt, variables: &mut Vec<String>, lists: &mut Vec<String>, 
 }
 
 /// Translates an expr
-pub fn texpr(expr: TExpr, variables: &mut Vec<String>, lists: &mut Vec<String>, stmts: &mut Vec<Statement>) -> Expr {
+pub fn texpr(expr: TExpr, stmts: &mut Vec<Statement>) -> Expr {
     
     use TExpr as E;
     match expr {
@@ -95,12 +89,12 @@ pub fn texpr(expr: TExpr, variables: &mut Vec<String>, lists: &mut Vec<String>, 
         E::Block(block) => {
             // append all of the block's statements
             for ((stmt, _), _) in block.stmts {
-                tstmt(stmt, variables, lists, stmts);
+                tstmt(stmt, stmts);
             }
 
             // return tail statement
             match block.tail {
-                Some(((TStmt::Expr(tail), _), _)) => texpr(tail, variables, lists, stmts),
+                Some(((TStmt::Expr(tail), _), _)) => texpr(tail, stmts),
                 _ => Expr::String("<nil>".to_string()),
             }
         },
@@ -110,11 +104,11 @@ pub fn texpr(expr: TExpr, variables: &mut Vec<String>, lists: &mut Vec<String>, 
             use crate::lang::typed::builtin::TBuiltinFnCall as B;
             match *call {
                 // convert the `as_str` builtin to it's scratch counterpart
-                B::AsString((expr, _)) => texpr(expr, variables, lists, stmts),
+                B::AsString((expr, _)) => texpr(expr, stmts),
 
                 // convert the `input` builtin to it's scratch counterpart
                 B::Input((expr, _)) => {
-                    let prompt = texpr(expr, variables, lists, stmts);
+                    let prompt = texpr(expr, stmts);
                     stmts.push(Statement::Ask { prompt });
                     Expr::Answer
                 },
@@ -125,7 +119,7 @@ pub fn texpr(expr: TExpr, variables: &mut Vec<String>, lists: &mut Vec<String>, 
                 // convert the `println` builtin to it's scratch counterpart
                 B::PrintLn(args) => {
                     let stmt = match args {
-                        Some((expr, _)) => Statement::PushList { ident: "console".to_string(), value: texpr(expr, variables, lists, stmts) },
+                        Some((expr, _)) => Statement::PushList { ident: "console".to_string(), value: texpr(expr, stmts) },
                         None => Statement::PushList { ident: "console".to_string(), value: Expr::String(String::new()) },
                     };
 
@@ -139,16 +133,16 @@ pub fn texpr(expr: TExpr, variables: &mut Vec<String>, lists: &mut Vec<String>, 
         E::Concat(lhs, rhs) => {
             let (((lhs, _), _), ((rhs, _), _)) = (*lhs, *rhs);
 
-            let lhs = texpr(lhs, variables, lists, stmts);
-            let rhs = texpr(rhs, variables, lists, stmts);
+            let lhs = texpr(lhs, stmts);
+            let rhs = texpr(rhs, stmts);
 
             Expr::Concat(Box::new(lhs), Box::new(rhs))
         },
 
         // maths single-space
-        E::Pos(expr) => texpr(expr.0.0, variables, lists, stmts),
+        E::Pos(expr) => texpr(expr.0.0, stmts),
         E::Neg(expr) => Expr::Mul(
-            Box::new(texpr(expr.0.0, variables, lists, stmts)),
+            Box::new(texpr(expr.0.0, stmts)),
             Box::new(Expr::Integer(-1)),
         ),
 
@@ -156,41 +150,48 @@ pub fn texpr(expr: TExpr, variables: &mut Vec<String>, lists: &mut Vec<String>, 
         E::Add(lhs, rhs) => {
             let (((lhs, _), _), ((rhs, _), _)) = (*lhs, *rhs);
 
-            let lhs = texpr(lhs, variables, lists, stmts);
-            let rhs = texpr(rhs, variables, lists, stmts);
+            let lhs = texpr(lhs, stmts);
+            let rhs = texpr(rhs, stmts);
 
             Expr::Add(Box::new(lhs), Box::new(rhs))
         },
         E::Sub(lhs, rhs) => {
             let (((lhs, _), _), ((rhs, _), _)) = (*lhs, *rhs);
 
-            let lhs = texpr(lhs, variables, lists, stmts);
-            let rhs = texpr(rhs, variables, lists, stmts);
+            let lhs = texpr(lhs, stmts);
+            let rhs = texpr(rhs, stmts);
 
             Expr::Sub(Box::new(lhs), Box::new(rhs))
         },
         E::Mul(lhs, rhs) => {
             let (((lhs, _), _), ((rhs, _), _)) = (*lhs, *rhs);
 
-            let lhs = texpr(lhs, variables, lists, stmts);
-            let rhs = texpr(rhs, variables, lists, stmts);
+            let lhs = texpr(lhs, stmts);
+            let rhs = texpr(rhs, stmts);
 
             Expr::Mul(Box::new(lhs), Box::new(rhs))
         },
         E::Div(lhs, rhs) => {
             let (((lhs, _), _), ((rhs, _), _)) = (*lhs, *rhs);
 
-            let lhs = texpr(lhs, variables, lists, stmts);
-            let rhs = texpr(rhs, variables, lists, stmts);
+            let lhs = texpr(lhs, stmts);
+            let rhs = texpr(rhs, stmts);
 
             Expr::Div(Box::new(lhs), Box::new(rhs))
         },
 
         // Getting Variables
-        E::VarGet { ident, .. } => Expr::Variable { ident },
+        E::VarGet { ident, var_type } => {
+            // if list, use placeholder
+            if let Type::List(_) = var_type {
+                Expr::String("<list>".to_string())
+            } else {
+                Expr::Variable { ident }
+            }
+        },
 
         // Lists
-        E::List(_, _) => unreachable!(),
+        E::List(_, _) => Expr::String("<list>".to_string()),
 
         _ => todo!(),
     }
